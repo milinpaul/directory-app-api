838805588c49a86a498110829dc4ff0f0f996698
Milin Paul <milin.paul@experionglobal.com>
2020-02-02 20:00:08 +0530

    handle invalid id error in mongoose

--- app/modules/listings/updateListingService.js
+++ app/modules/listings/updateListingService.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose')
+
 const Listing = require('../../Schemas/ListingSchema')
 
 const updateListingService = async (req, res, next) => {
@@ -6,6 +8,13 @@ const updateListingService = async (req, res, next) => {
     body
   } = req
   try {
+    if (!mongoose.Types.ObjectId.isValid(listingId)) {
+      return res.status(404).json({
+        error: true,
+        message: 'Cannot find the listingId'
+      })
+    }
+
     const updatedListing = await Listing.updateOne({ _id: listingId }, { $set: { ...body } })
     res.status(200).json(updatedListing)
   } catch (error) {
